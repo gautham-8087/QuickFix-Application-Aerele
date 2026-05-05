10f43f4deb3d558cf932461486839ec0b9f6c4c4
gautham-8087 <agautham2024@gmail.com>
2026-05-05 18:20:55 +0530

    fix: resolve the linter issue

--- quickfix/setup.py
+++ quickfix/setup.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe import _
 
 
 def after_install():
@@ -19,11 +20,11 @@ def after_install():
 			}
 		).insert(ignore_permissions=True)
 
-	frappe.msgprint("QuickFix setup completed successfully")
+	frappe.msgprint(_("QuickFix setup completed successfully"))
 
 
 def before_uninstall():
 	submitted_jobs = frappe.db.exists("Job Card", {"docstatus": 1})
 
 	if submitted_jobs:
-		frappe.throw("Cannot uninstall: Submitted Job Cards exist")
+		frappe.throw(_("Cannot uninstall: Submitted Job Cards exist"))
